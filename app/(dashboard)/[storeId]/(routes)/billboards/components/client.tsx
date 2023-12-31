42f69bb4a63c714d8ceccd410ef33a460311289b
mg143pavankumar <pavansharma.mg143@gmail.com>
2023-12-31 02:53:55 -0500

    added: Apilist in billboards page

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -9,6 +9,7 @@ import { Separator } from "@/components/ui/separator";
 import { FC } from "react";
 import { BillboardColumn, columns } from "./columns";
 import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
 
 interface BillboardClientProps {
   data: BillboardColumn[];
@@ -35,6 +36,8 @@ export const BillboardClient: FC<BillboardClientProps> = ({ data }) => {
 
       <Separator />
       <DataTable columns={columns} data={data} searchKey="label" />
+      <Heading title="API" description="API calls for Billboards" />
+      <ApiList entityName="billboards" entityIdName="billboardId" />
     </>
   );
 };
